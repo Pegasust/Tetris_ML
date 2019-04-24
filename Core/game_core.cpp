34979b3b169769edee3b8320a8aeb7293a24952b
Hung Tran <Pegasucksgg@Gmail.com>
2019-04-24 19:05:42 -0400

    Implemented Game's RNG. Uses xorshift algorithm because it can be manipulated or randomized for the seed, which is what we want for machine learnings.

--- Core/game_core.cpp
+++ Core/game_core.cpp
@@ -1,2 +1,28 @@
 #include "game_core.h"
 
+GameCore::GameModule::GameModule(char args[])
+{
+	//Normal initialization
+	//Classic tetris
+	GameRule* classic_rule;
+	classic_rule->max_x = 10;
+	classic_rule->max_y = 20;
+	classic_rule->n_pieces_knowahead = 1;
+	info->rule = classic_rule;
+}
+bool GameCore::GameModule::try_update()
+{
+
+}
+
+void GameCore::GameModule::start_game(GameRNG::RNGSeed seed)
+{
+
+}
+
+GameRNG::RNGSeed GameCore::GameModule::start_game()
+{
+	GameRNG::RNGSeed initial_seed = GameRNG::generate_random_seed();
+	start_game(initial_seed);
+	return initial_seed;
+}
--- Core/game_core.h
+++ Core/game_core.h
@@ -1,25 +1,78 @@
 #pragma once
 #include <queue>
 #include <vector>
+#include <stdint.h>
+#include "core_timing.h"
 #include "tetris_piece.h"
 using namespace GameFeatures;
 namespace GameCore
 {
-	class GameModule
-	{
-	public:
-		GameInfo* info;
-	};
 	struct GameInfo
 	{
-		GameRule* rule;
+		const GameRule* rule;
 		std::queue<TetrisPiece> next_pieces;
 		TetrisPiece* current_piece;
 		std::vector<TetrisPiece> built_pieces;
 	};
 	struct GameRule
 	{
-		const int n_pieces_knowahead;
-		const int max_x, max_y;
+		//The number of pieces shown on the top right corner
+		int n_pieces_knowahead;
+		//length or width of the game
+		int max_x, max_y;
+	};
+	//I am using the xorshift algorithm because it can be manipulated, which might be useful
+	//for machine learnings that want to learn single seed run (back-tracking).
+	static class GameRNG
+	{
+	public:
+		//I use 64-bit because its max value (18,446,744,073,709,551,615) is enough randomness
+		typedef uint64_t RNGSeed;
+		typedef std::chrono::steady_clock RNGClock;
+		//https://en.wikipedia.org/wiki/Xorshift
+		//One of the fastest non-crypto rng algorithm.
+		static RNGSeed xorshift64(RNGSeed state[1])
+		{
+			RNGSeed x = state[0];
+			x ^= x << 13;
+			x ^= x >> 7;
+			x ^= x << 17;
+			return state[0] = x;
+		}
+		//Based on time
+		static RNGSeed generate_random_seed()
+		{
+			//Get time now
+			auto invoke_time = RNGClock::now();
+			//Hardcoded because 0xDEADBEEF is a valid number : ^)
+			RNGSeed x[1] = { 0xDEADBEEF };
+			//Have it undergo some task to eliminate the upcoming RNGClock::now() the exact same
+			x[0] = xorshift64(x);
+			//Get the time span (this is pseudo-random due to the various clock speed)
+			auto time_span = RNGClock::now() - invoke_time;
+			//In nanosecond => It is actually pretty hard to recreate this randomness
+			RNGSeed y = time_span.count();
+			//Divide by two on each because the two values might be too big and makes it out of 
+			//64 bit boundary.
+			//Moreover, since there is a very small chance, but is likely to happen, that y = 0.
+			//We don't want that to happen since the xorshift will produce only 0s if
+			//we throw a 0 as the seed.
+			return (x[0] / 2) + (y / 2);
+		}
+	};
+
+	//Game engine
+	class GameModule
+	{
+	public:
+		GameInfo* info;
+		//Initialize
+		GameModule(char args[]);
+		//Main loop; return false if there is a certain error
+		bool try_update();
+		//Manipulated rng
+		void start_game(GameRNG::RNGSeed seed);
+		//The real rng
+		GameRNG::RNGSeed start_game();
 	};
 }
