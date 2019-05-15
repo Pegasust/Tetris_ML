51a044f1bfb29d3a99befae6e3a36ceca3a26ff3
Hung Tran <Pegasucksgg@Gmail.com>
2019-05-15 13:40:11 -0400

    CMake

---
+++ Cmake_Zenix/Cmake_Zenix.cpp
@@ -0,0 +1,12 @@
+﻿// Cmake_Zenix.cpp : Defines the entry point for the application.
+//
+
+#include "Cmake_Zenix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
